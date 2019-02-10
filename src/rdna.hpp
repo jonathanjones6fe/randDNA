1af5fe0d4ea6e37086060399ede326660e5a8020
jonathanjones6fe <jonathanjones6fe+@users.noreply.github.com>
2019-02-10 14:47:06 -0400

    create function randDNA

---
+++ src/rdna.hpp
@@ -0,0 +1,18 @@
+#include <random>
+#include <string>
+
+using namespace std;
+
+string randDNA (int seed, string bases, int n) {
+	if (bases.length() < 1) //nothing to use 
+		return "";
+		
+	mt19937 random(seed);	
+  	uniform_int_distribution<> distro(0, bases.length()-1);
+	string dna = "";
+	
+	for(int i = 0; i < n; i++)
+		dna += bases[distro(random)]; //randomly add a chars to dna from base
+
+  	return dna;
+}
